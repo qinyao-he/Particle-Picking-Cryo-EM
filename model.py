9cd3704e8dea13e34fea147bc8b42ff1b3372c8c
何钦尧 <hqythu@gmail.com>
2016-04-12 13:43:28 +0800

    cnn deep

--- model.py
+++ model.py
@@ -106,68 +106,6 @@ def build_cnn():
     return model
 
 
-def build_cnn_bn():
-    model = Sequential()
-
-    # 64 * 64 * 1
-    model.add(Convolution2D(8, 5, 5, border_mode='same',
-                            init='he_uniform', subsample=(2, 2),
-                            input_shape=(1, 64, 64)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    # 32 * 32 * 8
-
-    model.add(Convolution2D(8, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Convolution2D(8, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # 16 * 16 * 8
-
-    model.add(Convolution2D(16, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Convolution2D(16, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # 8 * 8 * 16
-
-    model.add(Convolution2D(16, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Convolution2D(16, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # 4 * 4 * 32
-
-    model.add(Convolution2D(32, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Convolution2D(32, 3, 3, border_mode='same',
-                            init='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # 2 * 2 * 32
-
-    model.add(Flatten())
-    model.add(Dense(128, init='he_uniform', activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-
-    return model
-
-
 def build_cnn_small():
     model = Sequential()
 
@@ -200,6 +138,64 @@ def build_cnn_small():
     model.add(Flatten())
     model.add(Dropout(0.5))
     model.add(Dense(128, init='he_uniform', activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    return model
+
+
+def build_cnn_deep():
+    model = Sequential()
+
+    # 64 * 64 * 1
+    model.add(Convolution2D(32, 5, 5, border_mode='same', activation='relu',
+                            init='he_uniform', input_shape=(1, 64, 64)))
+    model.add(Convolution2D(32, 5, 5, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # 32 * 32 * 32
+
+    model.add(Convolution2D(32, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # 16 * 16 * 32
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # 8 * 8 * 64
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # 4 * 4 * 64
+
+    model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu',
+                            init='he_uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # 2 * 2 * 128
+
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(512, init='he_uniform', activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1, activation='sigmoid'))
 
