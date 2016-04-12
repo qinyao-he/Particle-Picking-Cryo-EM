942f5e9e4cee68add3a2e1ca355f3108f3b469c8
何钦尧 <hqythu@gmail.com>
2016-04-12 16:51:22 +0800

    user the first 1000 sample for PCA

--- svm.py
+++ svm.py
@@ -8,13 +8,6 @@ from sklearn import decomposition
 from load import load_data
 
 
-def pca(X):
-    pca = decomposition.PCA(n_components=25*25)
-    pca.fit(X)
-    X = pca.transform(X)
-    return X
-
-
 def main():
     six.print_('loading data')
     train_x, train_y, val_x, val_y = load_data()
@@ -22,14 +15,18 @@ def main():
     val_x = val_x.reshape(-1, 64 * 64)
     six.print_('load data complete')
 
-    train_x = pca(train_x)
+    six.print_('start PCA')
+    pca = decomposition.PCA(n_components=25*25)
+    pca.fit(train_x[:1000])
+    train_x = pca.transform(train_x)
     six.print_('PCA complete')
 
-    clf = SVC(C=0.001, verbose=True, max_iter=100)
+    clf = SVC(C=0.001, verbose=True)
     six.print_('start training')
     clf.fit(train_x, train_y)
     six.print_('training complete')
 
+    val_x = pca.transform(val_x)
     acc = sum(val_y == clf.predict(val_x)) / float(len(val_y))
     print(acc)
 
